cc97e0cd62ca0488c43b56d14fd9f2efe823d499
chris <Christopher@developer.com>
2021-10-01 06:43:29 +0800

    - sample app template for a laravel and vue setup.

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        <b-button
+                            @click="clicked">
+                            Request from Api
+                        </b-button>
+                        <br />
+                        Response: 
+                        {{sample}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                "sample": {}
+            };
+        },
+        mounted() {
+            console.log('Component mounted.');
+        },
+        methods: {
+            clicked () {
+                var self = this;
+                axios.get('/api/sample').then(function (response) {
+                    var data = response.data;
+                    
+                    self.sample = data.data.concat(['this is a sample api response you can play with']);
+                });
+            }
+        }
+    }
+</script>
